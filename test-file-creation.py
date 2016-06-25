381da2dcf564c8ae02ea5631e373687612b9bbe8
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-06-25 20:59:23 +1200

    Parameters for script test-file-creation.py

--- test-file-creation.py
+++ test-file-creation.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Test File Creation in FTP servers')
+parser.add_argument('--server', help='Server Name or IP Address')
+parser.add_argument('--port', help='Server Port (defaults to 22)', type=int, default=22)
+
+
 def main():
-    pass
+    args = parser.parse_args()
+    print(args.port)
 
 if __name__ == '__main__':
     main()
